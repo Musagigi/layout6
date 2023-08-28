43c5bff7b6b3d3296ca380e661a3dee373acd0b0
Musajiji <dzhal@bk.ru>
2023-08-29 01:02:56 +0300

    разбил на компоненты

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import "../css/main.css";
 import { useEffect, useState } from "react";
-// import Fashion from "./pages/fashion/Fashion";
 
 const categoryId = {
 	index: 0,
@@ -17,6 +16,84 @@ const categoryName = {
 	politics: "политика",
 	sport: "спорт",
 };
+
+function Navigation({ onNavClick, currentCategory }) {
+	return (
+		<>
+			<a
+				className="mr-[50px] shrink-0"
+				href="/"
+				data-href="index"
+				onClick={onNavClick}
+			>
+				<img
+					className="w-full h-[90px]"
+					src="/img/logo.svg"
+					alt="logo"
+				/>
+			</a>
+			<nav>
+				<ul className="flex gap-[60px] font-bold text-2xl">
+					{["index", "fashion", "tech", "politics", "sport"].map((element) => {
+						return (
+							<li key={element}>
+								<a
+									className=" hover:text-orange"
+									href="#"
+									data-href={element}
+									onClick={onNavClick}
+								>
+									{currentCategory[element]}
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+		</>
+	);
+}
+
+function MainArticle({ title, image, category, description, source }) {
+	return (
+		<article className="flex flex-row-reverse gap-5">
+			<div className="flex flex-col justify-between shrink grow-[3] basis-2/3">
+				<h1 className="text-[42px] leading-[50px] font-medium">{title}</h1>
+				<span className="text-sm leading-6 font-medium text-orange uppercase -order-1">
+					{category}
+				</span>
+				<p className="leading-[26px] mt-8">{description}</p>
+				<span className="text-sm leading-none font-normal text-grey uppercase">
+					{source}
+				</span>
+			</div>
+			<div className="grow shrink-0 basis-1/3">
+				<img
+					className="object-cover"
+					src={encodeURI(image)}
+					alt="article image"
+				/>
+			</div>
+		</article>
+	);
+}
+
+function SecondArticle({ title, date, source }) {
+	<li>
+		<article className="border-l-8 pl-2">
+			<h2>{title}</h2>
+			<span className="text-xl font-normal leading-6 text-orange uppercase">
+				{new Date(date).toLocaleDateString("ru-RU", {
+					month: "long",
+					day: "numeric",
+				})}
+			</span>
+			<span className="ml-1.5 text-xl font-normal leading-6 text-grey uppercase">
+				{source}
+			</span>
+		</article>
+	</li>;
+}
 
 export default function Home() {
 	const [category, setCategory] = useState("index");
@@ -42,112 +119,52 @@ export default function Home() {
 			});
 	}, [category]);
 
-	console.log(articles);
-
 	return (
 		<div className="container max-2xl:max-w-none h-screen grid grid-rows-layout gap-[135px]">
 			<header className="flex mt-9">
-				<a
-					className="mr-[50px] shrink-0"
-					href="/index.html"
-					data-href="index"
-					onClick={onNavClick}
-				>
-					<img
-						className="w-full h-[90px]"
-						src="/img/logo.svg"
-						alt="logo"
-					/>
-				</a>
-				<nav>
-					<ul className="flex gap-[60px] font-bold text-2xl">
-						{["index", "fashion", "tech", "politics", "sport"].map(
-							(element) => {
-								return (
-									<li key={element}>
-										<a
-											className=" hover:text-orange"
-											href="#"
-											data-href={element}
-											onClick={onNavClick}
-										>
-											{categoryName[element]}
-										</a>
-									</li>
-								);
-							}
-						)}
-					</ul>
-				</nav>
+				<Navigation
+					onNavClick={onNavClick}
+					currentCategory={categoryName}
+				/>
 			</header>
 
 			<main className="flex justify-between gap-[90px]">
-				<section
-					id="main-news"
-					className="flex flex-col gap-[50px]"
-				>
+				<section className="flex flex-col gap-[50px]">
 					{articles.items.slice(0, 3).map((item) => {
 						return (
-							<article
+							<MainArticle
 								key={item.id}
-								className="flex flex-row-reverse gap-5"
-							>
-								<div className="flex flex-col justify-between shrink grow-[3] basis-2/3">
-									<h1 className="text-[42px] leading-[50px] font-medium">
-										{item.title}
-									</h1>
-									<span className="text-sm leading-6 font-medium text-orange uppercase -order-1">
-										{
-											articles.categories.find(
-												(catId) => catId.id === item.category_id
-											).name
-										}
-									</span>
-									<p className="leading-[26px] mt-8">{item.description}</p>
-									<span className="text-sm leading-none font-normal text-grey uppercase">
-										{
-											articles.sources.find(
-												(source) => source.id === item.source_id
-											).name
-										}
-									</span>
-								</div>
-								<div className="grow shrink-0 basis-1/3">
-									<img
-										className="object-cover"
-										src={encodeURI(item.image)}
-										alt="article image"
-									/>
-								</div>
-							</article>
+								title={item.title}
+								image={item.image}
+								description={item.description}
+								source={
+									articles.sources.find(
+										(source) => source.id === item.source_id
+									).name
+								}
+								category={
+									articles.categories.find(
+										(catId) => catId.id === item.category_id
+									).name
+								}
+							/>
 						);
 					})}
 				</section>
 				<aside>
-					<ul
-						id="second-news"
-						className="flex flex-col gap-[35px]"
-					>
+					<ul className="flex flex-col gap-[35px]">
 						{articles.items.slice(3, 12).map((item) => {
 							return (
-								<li key={item.id}>
-									<article className="border-l-8 pl-2">
-										<h2>{item.title}</h2>
-										<span className="text-xl font-normal leading-6 text-orange uppercase">
-											{new Date(item.date).toLocaleDateString("ru-RU", {
-												month: "long",
-												day: "numeric",
-											})}
-										</span>
-										<span className="ml-1.5 text-xl font-normal leading-6 text-grey uppercase">
-											{
-												articles.sources.find(
-													(source) => source.id === item.source_id
-												).name
-											}
-										</span>
-									</article>
-								</li>
+								<SecondArticle
+									key={item.id}
+									title={item.title}
+									date={item.date}
+									source={
+										articles.sources.find(
+											(source) => source.id === item.source_id
+										).name
+									}
+								/>
 							);
 						})}
 					</ul>
@@ -155,38 +172,10 @@ export default function Home() {
 			</main>
 
 			<footer className="relative text-white before:bg-dark before:w-[100vw] before:absolute before:top-[0] before:bottom-[0] before:right-[0] before:left-[calc((-100vw+100%)/2)] before:-z-10">
-				<div className="flex items-center mt-[22px]">
-					<a
-						className="mr-[50px] shrink-0"
-						href="/index.html"
-					>
-						<img
-							className="w-full h-[33px]"
-							src="/img/logo.svg"
-							alt="logo"
-						/>
-					</a>
-					<nav>
-						<ul className="flex gap-[60px] font-bold text-2xl">
-							{["index", "fashion", "tech", "politics", "sport"].map(
-								(element) => {
-									return (
-										<li key={element}>
-											<a
-												className=" hover:text-orange"
-												href="#"
-												data-href={element}
-												onClick={onNavClick}
-											>
-												{categoryName[element]}
-											</a>
-										</li>
-									);
-								}
-							)}
-						</ul>
-					</nav>
-				</div>
+				<Navigation
+					onNavClick={onNavClick}
+					currentCategory={categoryName}
+				/>
 				<div className="flex mt-12">
 					<div className="mr-auto">
 						Сделано на Frontend курсе в
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from "vite";
-const { resolve } = require('path')
+import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-	root: './src',
-	base: '',
-	build: {
-		outDir: './../dist', //  путь относительно root
-		emptyOutDir: true,
-		rollupOptions: {
-			input: {
-				main: resolve('./src/index.html'),
-				fashion: resolve('./src/pages/fashion.html'),
-				politics: resolve('./src/pages/politics.html'),
-				sport: resolve('src/pages/sport.html'),
-				tech: resolve('./src/pages/tech.html'),
-			}
-		}
-	},
-	publicDir: './../public',
+	plugins: [react()]
 })
 
 
